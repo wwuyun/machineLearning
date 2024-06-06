9e3cc8ecd30e416c9f5d3778e5853b6b3c536723
wwuyun <58965420+wwuyun@users.noreply.github.com>
2024-06-06 14:12:34 +0800

    Update Feedforward_neural_network.py

--- Feedforward_neural_network.py
+++ Feedforward_neural_network.py
@@ -1,11 +1,12 @@
-# 多种SVM核函数对比
+# 前馈神经网络
+import tensorflow as tf
+from tensorflow.keras import layers, models
 import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from skimage.feature import hog
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report
-from sklearn import svm
+from sklearn.datasets import fetch_openml
 
 # 加载FashionMNIST数据集
 mnist = fetch_openml("Fashion-MNIST", version=1)
@@ -23,25 +24,31 @@ X_reshaped = X_array.reshape(-1, 28, 28)
 # 划分数据集
 X_train, X_test, y_train, y_test = train_test_split(X_reshaped, y, test_size=0.3, random_state=42)
 
-# 特征提取：使用HOG
-def extract_features(X):
-    features = []
-    for image in X:
-        hog_features = hog(image, pixels_per_cell=(14, 14), cells_per_block=(1, 1), orientations=9, block_norm='L2', visualize=False, transform_sqrt=True)
-        features.append(hog_features)
-    return np.array(features)
+# 定义神经网络模型
+def create_nn_model(input_shape):
+    model = models.Sequential([
+        layers.Flatten(input_shape=input_shape),  # 将输入数据展平
+        layers.Dense(128, activation='relu'),     # 全连接层，128个神经元，ReLU激活函数
+        layers.Dropout(0.2),                      # Dropout 层，防止过拟合
+        layers.Dense(10, activation='softmax')   # 输出层，10个神经元，softmax激活函数
+    ])
+    return model
 
-X_train_features = extract_features(X_train)
-X_test_features = extract_features(X_test)
+# 创建模型实例
+input_shape = (28, 28)
+model = create_nn_model(input_shape)
 
-# 使用不同类型的SVM核函数进行分类并打印结果
-for kernel in ['linear', 'poly', 'rbf']:
-    print(f"Using SVM with {kernel} kernel:")
-    # 创建SVM分类器
-    svm_classifier = svm.SVC(kernel=kernel)
-    # 训练模型
-    svm_classifier.fit(X_train_features, y_train)
-    # 预测
-    y_pred = svm_classifier.predict(X_test_features)
-    # 打印分类性能指标
-    print(classification_report(y_test, y_pred))
+# 编译模型
+model.compile(optimizer='adam',                        # 优化器：Adam
+              loss='sparse_categorical_crossentropy',  # 损失函数：交叉熵损失
+              metrics=['accuracy'])                    # 评估指标：准确率
+
+# 查看模型摘要
+model.summary()
+
+# 训练模型
+history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+
+# 评估模型性能
+test_loss, test_acc = model.evaluate(X_test, y_test)
+print('Test accuracy:', test_acc)
